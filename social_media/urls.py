61e94010bed00c7d854219bd70e4da0487682086
Alex <moleksii@gmail.com>
2024-03-19 13:25:19 +0200

    Creating urls

---
+++ social_media/urls.py
@@ -0,0 +1,15 @@
+from rest_framework.routers import DefaultRouter
+
+from social_media.views import (
+    ProfileViewSet,
+    PostViewSet,
+)
+
+router = DefaultRouter()
+router.register(r"profiles", ProfileViewSet)
+router.register(r"posts", PostViewSet)
+
+
+urlpatterns = router.urls
+
+app_name = "social_media"

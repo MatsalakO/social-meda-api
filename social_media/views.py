5082b139bca6e976fd732407507ff4d8289e3451
Alex <moleksii@gmail.com>
2024-03-19 13:24:29 +0200

    Creating views for profile and posts

--- social_media/views.py
+++ social_media/views.py
@@ -1,3 +1,96 @@
-from django.shortcuts import render
+from rest_framework import viewsets, status
+from rest_framework.decorators import action
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
 
-# Create your views here.
+from social_media.models import (
+    Profile,
+    Post,
+)
+
+from social_media.serializers import (
+    ProfileListSerializer,
+    ProfileDetailSerializer,
+    ProfileSerializer,
+    ProfileImageSerializer,
+    PostSerializer,
+    PostListSerializer,
+    PostDetailSerializer,
+    PostImageSerializer,
+)
+
+
+class ProfileViewSet(viewsets.ModelViewSet):
+    queryset = Profile.objects.select_related("user")
+    permission_classes = [IsAuthenticated]
+
+    def get_serializer_class(self):
+        if self.action == "list":
+            return ProfileListSerializer
+
+        if self.action == "retrieve":
+            return ProfileDetailSerializer
+
+        if self.action == "upload_image":
+            return ProfileImageSerializer
+
+        return ProfileSerializer
+
+    def get_queryset(self):
+        username = self.request.query_params.get("username")
+        first_name = self.request.query_params.get("first_name")
+        last_name = self.request.query_params.get("last_name")
+        queryset = self.queryset
+
+        if username:
+            queryset = queryset.filter(
+                username__icontains=username
+            )
+        if first_name:
+            queryset = queryset.filter(
+                first_name__icontains=first_name
+            )
+        if last_name:
+            queryset = queryset.filter(
+                last_name__icontains=last_name
+            )
+
+        return queryset.distinct()
+
+
+class PostViewSet(viewsets.ModelViewSet):
+    queryset = Post.objects.select_related("user").prefetch_related("likes")
+    permission_classes = [IsAuthenticated, ]
+
+    def get_queryset(self):
+        queryset = Post.objects.all()
+        hashtag = self.request.query_params.get("hashtag", None)
+
+        if hashtag:
+            queryset = queryset.filter(content__icontains=f"{hashtag}")
+
+        return queryset
+
+    def get_serializer_class(self):
+        if self.action == "list":
+            return PostListSerializer
+        if self.action == "retrieve":
+            return PostDetailSerializer
+        if self.action == "upload_media":
+            return PostImageSerializer
+
+        return PostSerializer
+
+    @action(
+        methods=["POST"],
+        detail=True,
+        url_path="upload-image",
+    )
+    def upload_image(self, request, pk=None):
+        profile = self.get_object()
+        serializer = self.get_serializer(profile, data=request.data)
+
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
